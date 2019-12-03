5015bf4433ad64b6cde72bed8126bd380bcbc2ff
yamei <15001738114@163.com>
2019-12-03 16:53:23 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0, j = 10; i < j; i += 1) {
+            System.out.println(i + j);
+        }
+    }
+    
+}
